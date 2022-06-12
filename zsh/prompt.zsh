f60f914a2a3ebf6853ca8e84d7fd9641a08a88d1
Andres Osorio <andres.osoriogr@gmail.com>
2022-06-12 16:53:54 -0400

    zsh env modify

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,6 @@
 ##PROMPT='%B%(?.%F{#50FA7B}.%F{#FF6E67}) %F{#FF92D0}%1~ '
 PROMPT='%B%(?.%F{#50FA7B}.%F{#FF6E67}) '
-PROMPT+='%(?.%F{#50FA7B}.%F{#FF6E67})%b%f %F{#00FFFF}ﬦ '
+##PROMPT+='%(?.%F{#50FA7B}.%F{#FF6E67})%b%f %F{#00FFFF}ﬦ '
+PROMPT+='%F{#FF92D0}%b%f %F{#00FFFF}ﬦ '
 
 RPROMPT='%(?.%F{#50FA7B}.%F{#FF6E67}) %F{#949494}%T'
