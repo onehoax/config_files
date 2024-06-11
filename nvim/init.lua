06c468761721578973b0bae08912ba1a418f5889
Andres Osorio <andres.osoriogr@gmail.com>
2024-06-11 17:54:09 -0500

    fix: per directory .gitignore

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.autopairs"
-require "user.comment"
-require "user.cmp"
-require "user.lsp"
